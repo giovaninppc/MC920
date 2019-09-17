e842905fc1b1f9bd4b86a6699b510447c9670034
Giovani Pereira <-global>
2019-09-17 20:21:02 -0300

    Renamed proj1 to half tones

--- proj2/main.py
+++ proj2/main.py
@@ -5,17 +5,14 @@ from arguments import *
 
 def main():
     args = setupArguments()
-
     img = openImage(args.imagePath)
 
     for method in args.limiarizationMethods:
         out = limiarizate(img, method.method)
         saveImage('out/' + args.imageName + '_' + method.name + '_.pgm', img.astype(np.uint8))
 
-
 def limiarizate(img, method):
     return method(img)
-
 
 if __name__ == '__main__':
     main()
