fe2e6b32ded539a5de614affebebe8acb34c5695
Giovani Pereira <-global>
2019-10-01 19:02:01 -0300

    Added histogram method

--- limiarization/main.py
+++ limiarization/main.py
@@ -1,3 +1,6 @@
+import skimage
+import matplotlib
+import matplotlib.pyplot as plt
 import numpy as np
 
 from imageManipulation import *
@@ -9,12 +12,21 @@ def main():
 
     for method in args.limiarizationMethods:
         print('Applying ' + method.name)
+
         out = limiarizate(img, method.method)
         saveImage('out/' + args.imageName + '_' + method.name + '_.pgm', out.astype(np.uint8))
+        histogram(out)
         print('\nDone')
+
+    c = input()
 
 def limiarizate(img, method):
     return method(img.astype(int))
+
+def histogram(image):
+    # show histogram
+    plt.hist(image.ravel(), bins=32)
+    plt.show()
 
 if __name__ == '__main__':
     main()
