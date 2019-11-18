70ac0be4711a28c5e24708469c50fb7501396e85
Giovani Nascimento Pereira <giovani.x.pereira@gmail.com>
2019-11-18 00:23:24 -0300

    Created relatorios sobre

--- SDV/main.py
+++ SDV/main.py
@@ -3,6 +3,7 @@ from fileManipulation import *
 from PIL import Image
 import numpy as np
 import os
+import math
 import matplotlib.pyplot as plt
 
 
@@ -12,28 +13,54 @@ path = args.input_image_path
 output = args.output_path if args.output_path != None else 'compressed.png'
 
 img = Image.open(path)
+imgProperties = openImage(path)
 
-size = float(os.path.getsize(path))/1000
-print("Size(dimension): ",img.size)
-print("Original Image (%0.2f Kb):" %size)
+outimg = np.zeros(imgProperties.shape)
 
 # For each color channel
-for i in range(0,2):
-    imggray = img.convert('LA')
-    imgmat = np.array( list(imggray.getdata(band = 0)), float)
-    imgmat.shape = (imggray.size[1], imggray.size[0])
+for channel in range(0,3):
+    imgmat = np.array(list(img.getdata(band = channel)), float)
+    imgmat.shape = (img.size[1], img.size[0])
     imgmat = np.matrix(imgmat)
-    # plt.figure()
-    # plt.imshow(imgmat, cmap = 'gray')
-    # plt.title("Image after converting it into the Grayscale pattern")
-    # plt.show()
 
-    U, S, Vt = np.linalg.svd(imgmat) #single value decomposition
+    U, S, Vt = np.linalg.svd(imgmat) # single value decomposition
     for i in range(k):
         cmpimg = np.matrix(U[:, :i]) * np.diag(S[:i]) * np.matrix(Vt[:i,:])
-        # plt.imshow(cmpimg, cmap = 'gray')
-        # title = " Image after =  %s" %i
-        # plt.title(title)
-        # plt.show()
-        result = Image.fromarray((cmpimg ).astype(np.uint8))
-    result.save(output)
+        morphed = (cmpimg).astype(np.uint8)
+        result = Image.fromarray(morphed)
+
+    result.save('c' + str(channel) + '.png')
+
+redChannel = openImage('c0.png')
+blueChannel = openImage('c1.png')
+greenChannel = openImage('c2.png')
+
+
+for i in range(0, outimg.shape[0]):
+    for j in range(0, outimg.shape[1]):
+        outimg[i][j][0] = redChannel[i][j]
+        outimg[i][j][1] = blueChannel[i][j]
+        outimg[i][j][2] = greenChannel[i][j]
+
+
+os.remove('c0.png')
+os.remove('c1.png')
+os.remove('c2.png')
+
+saveImage(output, outimg)
+
+sizeIn = float(os.path.getsize(path))/1000
+sizeOut = float(os.path.getsize(output))/1000
+print("Original Image (%0.2f Kb)" %sizeIn)
+print("Compressed Image (%0.2f Kb)" %sizeOut)
+print("Comopression factor (p): %.3f" %(sizeOut/sizeIn))
+
+rmse = 0.0
+for i in range(0,outimg.shape[0]):
+    for j in range(0,outimg.shape[1]):
+        for k in range(0,outimg.shape[2]):
+            rmse += (imgProperties[i][j][k] - outimg[i][j][k]) ** 2
+rmse /= (outimg.shape[0] * outimg.shape[1] *outimg.shape[2])
+rmse = math.sqrt(rmse)
+
+print('RMSE: %.3f' %rmse)
